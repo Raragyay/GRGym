ef446ed89758d35b7232b4a905b91df9cfbc3fee
Raragyay <let987let987@gmail.com>
2020-04-02 05:03:32 -0400

    Added tests for hand.py shorthand

--- tests/hand/test_hand.py
+++ tests/hand/test_hand.py
@@ -15,12 +15,30 @@ def generate_has_card_data():
         with file_name.open(mode="rb") as file:
             test_hand = Hand()
             # noinspection PyTypeChecker
-            mask = np.load(file)
+            mask: np.ndarray = np.load(file)
             test_hand.cards = mask
             test_id = f"deck-{idx + 1}"
             test_data.append(pytest.param(test_hand, mask, id=test_id))
 
     return test_data
+
+
+def card_shorthand_idfn(val):
+    if isinstance(val, int):
+        return val
+    else:
+        return ""
+
+
+@pytest.mark.parametrize("card_val,expected",
+                         zip((1, 5, 9, 23, 46, 51), (f"2{Hand.suit_symbols[0]}",
+                                                     f"6{Hand.suit_symbols[0]}",
+                                                     f"10{Hand.suit_symbols[0]}",
+                                                     f"J{Hand.suit_symbols[1]}",
+                                                     f"8{Hand.suit_symbols[3]}",
+                                                     f"K{Hand.suit_symbols[3]}")), ids=card_shorthand_idfn)
+def test_card_shorthand(card_val: int, expected: str):
+    assert Hand.card_shorthand(card_val) == expected
 
 
 @pytest.mark.parametrize("starting_hand,expected", generate_has_card_data())
