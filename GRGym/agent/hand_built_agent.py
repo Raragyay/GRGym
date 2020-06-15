a4826653f6b0bec9a0588fb69c2486067920824e
Raragyay <let987let987@gmail.com>
2020-06-15 11:57:54 -0400

    Added hand_built_agent.py aversion to picking up own cards.
    Signed-off-by: Raragyay <let987let987@gmail.com>

--- GRGym/agent/hand_built_agent.py
+++ GRGym/agent/hand_built_agent.py
@@ -6,8 +6,16 @@ from GRGym.environment.card_state import CardState
 
 
 class HandBuiltAgent(BaseAgent):
+    def __init__(self):
+        self.visited = np.zeros((52,), dtype=np.bool)
+
+    def reset(self):
+        self.visited = np.zeros((52,), dtype=np.bool)
+
     def act(self, observation: np.ndarray) -> np.ndarray:
         my_cards = (observation[0:52] == CardState.MINE_FROM_DECK) | (observation[0:52] == CardState.MINE_FROM_DISCARD)
+        self.visited = self.visited | my_cards
+        # print(self.visited)
         my_cards = my_cards.reshape((4, 13)).astype(np.int)
         convolve_filter = np.zeros((7, 7))
         for i in range(7):
@@ -24,10 +32,14 @@ class HandBuiltAgent(BaseAgent):
         result = np.empty((56,))
         result[0:52] = (-convolved)
         top_of_discard = np.where(
-            (observation[0:52] == CardState.DISCARD_MINE_TOP) | (observation[0:52] == CardState.DISCARD_THEIRS_TOP))
-        # print(top_of_discard)
-        result[52] = convolved[top_of_discard[0][0]] if len(top_of_discard[0]) else 0
-        result[53] = np.mean(convolved[observation[0:52] == CardState.UNKNOWN])
+            (observation[0:52] == CardState.DISCARD_THEIRS_TOP))
+        # top_of_discard = np.where(
+        #     (observation[0:52] == CardState.DISCARD_MINE_TOP) | (observation[0:52] == CardState.DISCARD_THEIRS_TOP))
+        result[52] = np.mean(convolved[observation[0:52] == CardState.UNKNOWN])  # DECK
+        result[53] = convolved[top_of_discard[0][0]] if len(top_of_discard[0]) and not self.visited[top_of_discard[
+            0][0]] else 0  #
+        # DISCARD
+
         result[54] = 1
         result[55] = 0
         # print(result)
